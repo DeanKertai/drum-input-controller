785cb45751af081d6714c0f018cb7e3081acc1c9
Dean Kertai <deankertai@gmail.com>
2026-02-15 19:08:38 -0600

    Increase ADC speed

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,13 @@ TriggerInput inputs[] = {
     TriggerInput(0, A0),
 };
 
-void setup() { Comms::init(); }
+void setup() {
+    // Set prescaler to 32 (faster analogRead()).
+    // Should result in 500 kHz ADC clock (vs 125 kHz default)
+    ADCSRA = (ADCSRA & 0b11111000) | 0b101;
+
+    Comms::init();
+}
 
 void loop() {
     for (TriggerInput &input : inputs) {
